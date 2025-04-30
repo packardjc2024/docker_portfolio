740225782215b5a832d3669849fde69148f5a12b
Jeremy <packardjc@outlook.com>
2025-04-30 12:53:49 -0400

    flip cards ready

--- web/home_page/static/home_page/home_page.js
+++ web/home_page/static/home_page/home_page.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
+    // $('#flip-card').hover(function(){
+    //     flipCard();
+    // });
+
     setTimeout(function(){
         setupAnimation();
     }, 100);
+
+    setTimeout(function(){
+        typewriter();
+    }, 1000);
 });
+
 
 /******************************************************************************
 Logo animation related functions
@@ -117,120 +126,45 @@ function calculateLogoWidth(logosArray){
 }
 
 
-
-/*
-$(document).ready(function(){
+/******************************************************************************
+Typewriter related functions
+*******************************************************************************/
+function typewriter(){
+    let line1 = 'Hello, World!';
+    let line2 = "I'm Jeremy.";
+    let helloElement = $('#hello-container');
+    let nameElement = $('#name-container');
+    let speed = 100;
+    let duration = speed * line2.length + 2000;
+    type(helloElement, line1, speed);
     setTimeout(function(){
-        // animateLogos();
-        setupAnimation();
-    }, 100);
-});
-
-function setupAnimation(){
-    let containers = document.getElementsByClassName('logos-container');
-    for (let container of containers){
-        animateLogos(container);
-    }
-
+        helloElement.removeClass('caret-container');
+        nameElement.addClass('caret-container');
+        type(nameElement, line2, speed);
+    }, duration);
+    setInterval(function(){
+        // $('#name-container').removeClass('caret-container');
+        $('#welcome-container').attr('class', 'scroll');
+    }, duration * 2);
 }
 
-function animateLogos(containerElement){
-// Declares all the variables and calls all functions in the correct order
-    let logoElements = containerElement.getElementsByClassName('moving-logo');
-
-    let logos = [];
-    for (let logo of logoElements){
-        logos.push({
-            'element': logo,
-            'distanceX': 2,
-            'distanceY': 2,
-            'containerWidth': $('#logos-container').width(),
-            'containerHeight': $('#logos-container').height(),
-            'logoHeight': calculateLogoHeight(logoElements),
-            'logoWidth': calculateLogoWidth(logoElements)
-        });
-    }
-    
-    arrangeLogos(logos);
-
-    setTimeout(function(){
-        for (let logo of logos){
-            moveLogo(logo);
+function type(elementObject, elementString, speed){
+    let index = 0;
+    let typeInterval = setInterval(function(){
+        if (index <= elementString.length){
+            elementObject.html(elementString.slice(0, index));
+            index++;
         }
-    }, 3000);
+    }, speed);
 }
 
 
-function arrangeLogos(logosArray){
-// Arranges the logos' starting points based on the max size
-    let initialTop = 10;
-    let initialLeft = 10;
-    for (let i=0; i<logosArray.length; i++){
-        // Set every other logo to move the opposite direction
-        if (i % 2 != 0){
-            logosArray[i]['distanceX'] = -logosArray[i]['distanceX'];
-            logosArray[i]['distanceY'] = -logosArray[i]['distanceY'];
-        }
-        // Start a new row at the end of the container width
-        if (i != 0){
-            if (initialLeft + logosArray[i]['element'].offsetWidth >= logosArray[i]['containerWidth']){
-                initialLeft = 10;
-                initialTop = initialTop + logosArray[i]['logoHeight'] + 10;
-            }
-        }
-
-        // Make the initial placement
-        logosArray[i]['element'].style.left = initialLeft + 'px';
-        logosArray[i]['element'].style.top = initialTop + 'px';
-
-        // Update the intitial left position
-        initialLeft = initialLeft + logosArray[i]['logoWidth'] + 10;
+function flipCard(){
+    if ($('#flip-front').css('display') == 'flex'){
+        $('#flip-front').css('display', 'none');
+        $('#flip-back').css('display', 'flex');
+    } else {
+        $('#flip-front').css('display', 'flex');
+        $('#flip-back').css('display', 'none');
     }
 }
-
-
-function moveLogo(logoObject){
-    // Animation function that makes the logos move
-    let x = logoObject['element'].offsetLeft;
-    let y = logoObject['element'].offsetTop;
-
-    x += logoObject['distanceX'];
-    y += logoObject['distanceY'];
-
-    if (x + logoObject['element'].offsetWidth >= logoObject['containerWidth'] || x <= 0){
-        logoObject['distanceX'] = -logoObject['distanceX'];
-    }
-    if (y + logoObject['element'].offsetHeight >= logoObject['containerHeight'] || y <= 0){
-        logoObject['distanceY'] = -logoObject['distanceY'];
-    }
-
-    logoObject['element'].style.left = x + 'px';
-    logoObject['element'].style.top = y + 'px';
-
-    requestAnimationFrame(() => moveLogo(logoObject));
-}
-
-
-function calculateLogoHeight(logosArray){
-    // Returns the max height and width of the logos
-    let maxHeight = 0;
-    for (let logo of logosArray){
-        if (logo.offsetHeight > maxHeight){
-            maxHeight = logo.offsetHeight;
-        }
-    }
-    return maxHeight;
-}
-
-
-function calculateLogoWidth(logosArray){
-    // Returns the max height and width of the logos
-    let maxWidth = 0;
-    for (let logo of logosArray){
-        if (logo.offsetWidth > maxWidth){
-            maxWidth = logo.offsetWidth;
-        }
-    }
-    return maxWidth;
-}
-*/
--- web/static/project.js
+++ web/static/project.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
-    changeHeaderHeight();
-    changeFooterHeight();
-});
+// $(document).ready(function(){
+//     changeHeaderHeight();
+//     changeFooterHeight();
+// });
 
-$(window).on('resize', function(){
-    changeHeaderHeight();
-    changeFooterHeight();
-});
-
-
-function changeHeaderHeight(){
-    let headerHeight = $('.top-navbar').height();
-    $('#main').css('margin-top', `${headerHeight}px`);
-}
+// $(window).on('resize', function(){
+//     changeHeaderHeight();
+//     changeFooterHeight();
+// });
 
 
-function changeFooterHeight(){
-    let footerHeight = $('.bottom-navbar').height();
-    $('#main').css('margin-bottom', `${footerHeight}px`);
-}
+// function changeHeaderHeight(){
+//     let headerHeight = $('.top-navbar').height();
+//     $('#main').css('margin-top', `${headerHeight}px`);
+// }
+
+
+// function changeFooterHeight(){
+//     let footerHeight = $('.bottom-navbar').height();
+//     $('#main').css('margin-bottom', `${footerHeight}px`);
+// }
