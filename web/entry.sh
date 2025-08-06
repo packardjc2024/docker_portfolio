896264e160393330a74dda4270b6f1e78e77939a
Jeremy <packardjc@outlook.com>
2025-08-06 12:29:26 -0400

    .

--- web/entry.sh
+++ web/entry.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -x  # <-- this turns on shell debug logging
+
+echo "Running as $(whoami)"
+echo "Starting command: $@"
+
 # Collect static files
 python3 manage.py collectstatic --noinput
 
