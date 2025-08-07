9f225f1d899dec5b121847ed99bec3676933332c
Jeremy <packardjc@outlook.com>
2025-08-07 07:30:14 -0400

    .

--- web/entry.sh
+++ web/entry.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# Testing
+PATH_DIRECTORIES=(
+    '/var' '/var/lib' 
+    '/var/lib/docker' 
+    '/var/lib/docker/volumes'
+)
+VOLUME_DIRECTORIES=(
+    "/var/lib/docker/volumes/${PROJECT_NAME}_static_volume" 
+    "/var/lib/docker/volumes/${PROJECT_NAME}_media_volume" 
+    "/var/lib/docker/volumes/${PROJECT_NAME}_logs_volume" 
+)
+
+for path_directory in ${PATH_DIRECTORIES[@]}; do
+    sudo chown :staticgroup $path_directory
+    sudo chmod g+x $path_directory
+done
+
+for volume_directory in ${VOLUME_DIRECTORIES[@]}; do
+    sudo chown -R :staticgroup $volume_directory
+    sudo chmod -R g+xwr $volume_directory
+done
+# Testing
+
 # Collect static files
 python3 manage.py collectstatic --noinput
 
