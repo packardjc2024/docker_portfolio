b4cb9db590658041f7da20559bc496c1310d8885
Jeremy <packardjc@outlook.com>
2025-08-07 09:11:32 -0400

    .

--- web/entry.sh
+++ web/entry.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # Testing
-chown -R root:www-data /app/staticfiles
-chmod -R o+rx /app/staticfiles
+VOLUMES=(
+    "/app/staticfiles"
+    "/app/media"
+    "/app/logs"
+)
+
+for dir in "${VOLUMES[@]}"; do
+    mkdir -p "$dir"
+    chown -R root:www-data "$dir"
+    chmod -R g+rwX "$dir"
+done
 # Testing
 
 # Collect static files
